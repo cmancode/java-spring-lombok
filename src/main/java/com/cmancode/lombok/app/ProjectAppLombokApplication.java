1f31e054fad55ca4fcbd123455569d3c7415e807
cmancode <ing.mbcarlosa@gmail.com>
2020-01-30 23:58:59 -0500

    subiendo proyecto

---
+++ src/main/java/com/cmancode/lombok/app/ProjectAppLombokApplication.java
@@ -0,0 +1,13 @@
+package com.cmancode.lombok.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAppLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAppLombokApplication.class, args);
+	}
+
+}
